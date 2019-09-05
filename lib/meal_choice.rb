616021f330876797fd4095ad5a6ffdf6f4c41e99
Connor Meth <connormeth27@gmail.com>
2019-09-05 18:44:56 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,15 @@
-# Your code here
+def message
+  "What a nutritious meal!"
+end
+
+def meal_choice(veg1, veg2, protein = "meat")
+  # message = "What a nutritious meal!"
+  puts message
+  p "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+meal_choice("broccoli", "macaroni")
+
+meal_choice("broccoli", "macaroni", "tofu")
+
+
+# have to use a number (3) so that it's true/false 
