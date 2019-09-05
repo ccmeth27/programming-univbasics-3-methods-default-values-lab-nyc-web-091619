6fff220614a6ea429d240ab3c420ac9923e00d4a
Connor Meth <connormeth27@gmail.com>
2019-09-05 18:46:44 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,9 @@
-def meal_choice(veg1, veg2, protein = "meat")
+def message
   "What a nutritious meal!"
-  # puts message
+end
+
+def meal_choice(veg1, veg2, protein = "meat")
+  message
   p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 meal_choice("broccoli", "macaroni")
